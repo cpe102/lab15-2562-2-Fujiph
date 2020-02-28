fbdc82c8b5ed99eeac256728f6da65c14a75f3f5
lab302-37 <lab302-37@labeng.local>
2020-02-28 16:38:17 +0700

    v2

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -47,7 +47,7 @@ void showData(double *dPtr,int N,int M){
 void findRowSum(const double *dPtr,double *result,int N,int M){
 for (int i=0;i<N;i++){
 		for (int j=0;j<M;j++){
-			*(result+i) += *((dPtr+i)+j) ;
+			*(result+i) += *((dPtr+(i*M)+j)) ;
 		}
 	}
 }
